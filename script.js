88144134a2029f91d953376afe1eff9a87b2dcb9
Abhimanyu Mahto <abhimanyuverma677@gmail.com>
2024-01-19 15:36:49 +0530

    Completed the project upto sec 4 completed hover function to change color completed newgrid function to create new grid

--- script.js
+++ script.js
@@ -4,16 +4,47 @@ const container = document.createElement("div");
 container.id = "container";
 body.appendChild(container);
 
+
 let grid = 16;
-let containerWidth = (grid * grid);
+let boxSize = 20;
+let containerWidth = (grid * boxSize);
 container.style.width = `${containerWidth}px`;
 createGrid();
 
+// iterating a loop to create divS as boxes 
 function createGrid() {
      for (let i = 0; i < grid * grid; i++) {
              const divS = document.createElement("div");
-             divS.classList.add("gridStyle")
+             divS.setAttribute("style", `height: ${boxSize}px; width: ${boxSize}px;`);
+             divS.classList.add("gridStyle","hover");
              container.appendChild(divS);
      };
  };
  
+// step 3 hover function 
+
+let hoverDiv = document.querySelectorAll(".hover");
+
+hoverDiv.forEach(hoverDiv => {
+     hoverDiv.addEventListener("mouseover", () => {
+          hoverDiv.style.backgroundColor = "red";
+      });
+});
+
+
+// step 4 popup window
+
+const newGrid = document.querySelector("#newGrid");
+
+newGrid.addEventListener("click", () => {
+     let value = prompt("New Grid Size");
+     if(value > 100) {
+          alert("pls enter under 100 grids");
+          return;
+     };
+     container.innerHTML = "";
+     grid = value;
+     let containerWidth = (grid * boxSize);
+     container.style.width = `${containerWidth}px`;
+     createGrid();
+});
